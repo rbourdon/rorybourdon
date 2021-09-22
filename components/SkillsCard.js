8e5dfc48afd02f97584277564ef14806cf8a903b
Rory Bourdon <rorybourdon@gmail.com>
2021-09-22 09:25:13 -0400

    Refactor skills card

--- components/SkillsCard.js
+++ components/SkillsCard.js
@@ -1,17 +1,17 @@
 import styled, { ThemeContext } from "styled-components";
-import { motion } from "framer-motion";
+import { motion, MotionConfig } from "framer-motion";
 import NavCard from "@/components/NavCard";
 import SkillsIcon from "@/components/SkillsIcon";
 import { useContext } from "react";
-import Button from "@/components/Button";
 import SkillRoller from "@/components/SkillRoller";
+import Underline from "@/components/Underline";
 
 const CardContent = styled(motion.div)`
   width: 100%;
   height: 100%;
   display: grid;
   grid-template-columns: 100%;
-  grid-template-rows: 35% 10% 1fr calc(10% + 40px);
+  grid-template-rows: 35% 9% 1fr calc(9% + 40px);
   justify-items: center;
   align-items: center;
   position: relative;
@@ -21,16 +21,21 @@ const CardContent = styled(motion.div)`
 
 const Icon = styled(motion.div)`
   width: 90%;
-  height: 100%;
+  max-content;
   display: flex;
   align-items: center;
 `;
 
-const Label = styled(motion.p)`
+const Label = styled(motion.div)`
   width: 100%;
+  height: 100%;
   font-size: 1.35rem;
   font-weight: 300;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
 `;
 
 const skillsCardV = {
@@ -38,47 +43,61 @@ const skillsCardV = {
   visible: {
     opacity: 1,
     transition: {
-      delay: 3.45,
-      duration: 0.1,
+      delay: 710 * 230 * 0.0000012 + 2.05,
+      duration: 0.5,
     },
   },
 };
 
-export default function SkillsCard({ skills }) {
+const skillsIconV = {
+  hidden: {
+    pathLength: 0,
+  },
+  visible: {
+    pathLength: 1,
+    transition: {
+      delay: 710 * 230 * 0.0000012 + 2.15,
+      duration: 0.65,
+    },
+  },
+};
+
+export default function SkillsCard({ skills, selected, children }) {
   const theme = useContext(ThemeContext);
   return (
-    <NavCard
-      height={710}
-      width={230}
-      stemDir="h"
-      stemLoc={1}
-      stemSide="r"
-      stemLength={475}
-      color1={theme.red.get()}
-      color2={theme.yellow.get()}
-      effectLocation={{ x: 0.5, y: 0.25 }}
-      effectRadius={300}
-      id={"skillsCard"}
-      delay={0.5}
-    >
-      <CardContent variants={skillsCardV}>
-        <Icon>
-          <SkillsIcon />
-        </Icon>
-        <Label>Skills</Label>
-        <SkillRoller skills={skills} />
-        <Button
-          width={150}
-          height={50}
-          color1={theme.red.get()}
-          color2={theme.yellow.get()}
-          href="/"
-          id="skillsButton"
-          delay={3.5}
-        >
-          All Skills
-        </Button>
-      </CardContent>
-    </NavCard>
+    <MotionConfig transition={{ type: "spring", stiffness: 30 }}>
+      <NavCard
+        height={710}
+        width={230}
+        stemDir="h"
+        stemLoc={3}
+        stemLength={475}
+        color1={theme.blue.get()}
+        color2={theme.teal.get()}
+        effectOffset={{ x: 50, y: -50 }}
+        effectRadius={325}
+        id={"skills"}
+        delay={0.5}
+        tagline="I have a wide variety of skills and love to learn."
+        selected={selected}
+      >
+        <CardContent layoutId={"skillsCard_cardContent"} variants={skillsCardV}>
+          <Icon>
+            <SkillsIcon iconV={skillsIconV} />
+          </Icon>
+          <Label>
+            Skills
+            <Underline
+              width={50}
+              sWidth={1.2}
+              color1={theme.blue.get()}
+              color2={theme.teal.get()}
+            />
+          </Label>
+          <SkillRoller selected={selected} skills={skills} />
+          {children}
+        </CardContent>
+      </NavCard>
+    </MotionConfig>
   );
 }
