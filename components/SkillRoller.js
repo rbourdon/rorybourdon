4f4b5215084856b2387c6b99249e00171a2f755d
Rory Bourdon <rorybourdon@gmail.com>
2021-09-22 09:26:04 -0400

    Refactor skill roller

--- components/SkillRoller.js
+++ components/SkillRoller.js
@@ -7,30 +7,58 @@ const Container = styled(motion.div)`
   width: 100%;
   height: 100%;
   display: grid;
-  grid-template-rows: repeat(auto-fit, 40px);
+  grid-template-rows: repeat(auto-fit, 36px);
   grid-template-columns: 100%;
-  row-gap: 7px;
-  justify-content: center;
+  row-gap: 8px;
+  align-content: center;
   justify-items: center;
   align-items: center;
-  align-content: center;
   overflow: hidden;
 `;
 
-const rollerV = {
-  visible: { transition: { delayChildren: 1, staggerChildren: 1 } },
+const bubbleV = {
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const bubbleBottom = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const bubbleTop = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
 };
 
 function SkillBubbles({ skills, rollerPos }) {
   const skillBubbles = [];
 
-  for (let i = 0; i <= 3; i++) {
+  for (let i = 0; i <= 6; i++) {
     const index =
       rollerPos + i > skills.length - 1
         ? rollerPos + i - (skills.length - 1) - 1
         : rollerPos + i;
     skillBubbles.push(
-      <SkillBubble title={skills[index].title} key={skills[index].title}>
+      <SkillBubble
+        title={skills[index].title}
+        key={skills[index].title}
+        bubbleV={i === 0 ? bubbleTop : i === 6 ? bubbleBottom : bubbleV}
+      >
         {skills[index].title}
       </SkillBubble>
     );
@@ -39,20 +67,25 @@ function SkillBubbles({ skills, rollerPos }) {
   return skillBubbles;
 }
 
-export default function SkillRoller({ skills }) {
+export default function SkillRoller({ skills, selected }) {
   const [rollerPos, setRollerPos] = useState(0);
+  const [scrollTimer, setScrollTimer] = useState(null);
 
   const scrollSkills = () => {
     setRollerPos((prev) => (prev + 1 > skills.length - 1 ? 0 : prev + 1));
-    setTimeout(scrollSkills, 1300);
   };
 
   useEffect(() => {
-    setTimeout(scrollSkills, 1200);
-  }, []);
+    scrollTimer && clearInterval(scrollTimer);
+    !selected && setScrollTimer(setInterval(scrollSkills, 1100));
+    return function cleanup() {
+      clearInterval(scrollTimer);
+      setScrollTimer(null);
+    };
+  }, [selected]);
 
   return (
-    <Container variants={rollerV} initial="hidden" animate="visible">
+    <Container layoutId="skillRoller">
       <SkillBubbles skills={skills} rollerPos={rollerPos} />
     </Container>
   );
