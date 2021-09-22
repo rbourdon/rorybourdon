d9112d62ec6798605bc4217b7871b25b0b069561
Rory Bourdon <rorybourdon@gmail.com>
2021-09-22 09:29:09 -0400

    Add skills to index page

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import Layout from "@/components/layout";
 import Logo from "@/components/Logo";
 import NavCard from "@/components/NavCard";
 import SkillsCard from "@/components/SkillsCard";
+import ProjectsCard from "@/components/ProjectsCard";
 import styled, { ThemeContext } from "styled-components";
 import Button from "@/components/Button";
 import { motion } from "framer-motion";
 import { useContext, useEffect, useState } from "react";
 import Banner from "@/components/Banner";
 import { getAllSkillsTitles } from "@/lib/graphcms";
+import NavBar from "@/components/NavBar";
+import NavLink from "@/components//NavLink";
 
 const Container = styled(motion.div)`
   width: 100%;
@@ -16,6 +19,7 @@ const Container = styled(motion.div)`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  overflow: hidden;
 `;
 
 const IntroLogo = styled(motion.div)`
@@ -25,7 +29,7 @@ const IntroLogo = styled(motion.div)`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  position: fixed;
+  position: absolute;
   top: calc(50% - 175px);
   left: calc(50% - 175px);
 `;
@@ -50,7 +54,7 @@ const HeroBody = styled(motion.div)`
   height: max-content;
   display: flex;
   justify-content: end;
-  margin-top: 8vh;
+  margin-top: 1vh;
 `;
 
 const HeroSection = styled(motion.div)`
@@ -66,7 +70,7 @@ const Skills = styled(motion.div)`
   width: 100%;
   min-height: 50vh;
   padding: 150px 18vw;
-  height: max-content;
+  height: max(710px, 100vh);
   display: flex;
   justify-content: start;
 `;
@@ -89,71 +93,127 @@ const buttonsV = {
       delayChildren: 0.75,
     },
   },
+  exit: {
+    x: -500,
+    opacity: 0,
+    transition: { duration: 0 },
+  },
+};
+
+const welcomeCardV = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { delay: 200 * 275 * 0.0000012 + 1.65 },
+  },
+  exit: {
+    opacity: 0,
+    transition: { delay: 0, duration: 0 },
+  },
 };
 
 export default function Home({ skills }) {
+  const [selected, setSelected] = useState(false);
+  const [logoComplete, setLogoComplete] = useState(false);
   const [introComplete, setIntroComplete] = useState(false);
   const theme = useContext(ThemeContext);
+
+  const clickHandler = (selection) => () => {
+    setSelected(selection);
+    if (typeof window !== "undefined") {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }
+  };
+
   useEffect(() => {
-    setIntroComplete(true);
+    //setIntroComplete(true);
   }, []);
   return (
-    <Layout introComplete={introComplete}>
-      {!introComplete && (
-        <Container initial="hidden" animate="visible">
-          <IntroLogo layoutId="logo">
-            <Logo onAnimationComplete={() => setIntroComplete(true)} intro />
+    <Layout>
+      <NavBar logoComplete={logoComplete} setIntroComplete={setIntroComplete}>
+        <NavLink href="/">Projects</NavLink>
+        <NavLink href="/">Resume</NavLink>
+      </NavBar>
+      <Container>
+        {!introComplete && (
+          <IntroLogo initial="hidden" animate="visible" layoutId="logo">
+            <Logo onAnimationComplete={setLogoComplete} intro />
           </IntroLogo>
-        </Container>
-      )}
-      {introComplete && (
-        <Container initial="hidden" animate="visible">
-          <HeroSection>
-            <HeroBanner>
-              <Banner />
-              <Buttons variants={buttonsV}>
-                <Button
+        )}
+        {introComplete && (
+          <>
+            <HeroSection
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              style={{ opacity: selected ? 0 : 1 }}
+            >
+              <HeroBanner>
+                <Banner />
+                <Buttons variants={buttonsV}>
+                  <Button
+                    color1={theme.blue.get()}
+                    color2={theme.purple.get()}
+                    href="/"
+                    id="learnMoreButton"
+                    delay={1}
+                  >
+                    Learn More
+                  </Button>
+                  <Button
+                    color1={theme.blue.get()}
+                    color2={theme.teal.get()}
+                    href="/"
+                    id="contactButton"
+                    delay={1.5}
+                  >
+                    Contact Me
+                  </Button>
+                </Buttons>
+              </HeroBanner>
+              <HeroBody>
+                <NavCard
+                  height={200}
+                  width={275}
+                  stemDir="h"
+                  stemLoc={1}
+                  stemSide="l"
                   color1={theme.blue.get()}
                   color2={theme.purple.get()}
-                  href="/"
-                  id="learnMoreButton"
-                  delay={1}
+                  effectOffset={{ x: 90, y: 30 }}
+                  effectRadius={165}
+                  id={"welcome"}
+                  selected={selected}
                 >
-                  Learn More
-                </Button>
+                  <CardContent variants={welcomeCardV}>
+                    <p>Hello</p>
+                  </CardContent>
+                </NavCard>
+              </HeroBody>
+            </HeroSection>
+            )
+            <Skills layout>
+              <SkillsCard skills={skills} selected={selected}>
                 <Button
+                  width={150}
+                  height={50}
                   color1={theme.blue.get()}
                   color2={theme.teal.get()}
-                  href="/"
-                  id="contactButton"
-                  delay={1.5}
+                  href="/skills"
+                  onClick={clickHandler}
+                  id="skills"
+                  delay={710 * 230 * 0.0000012 + 2.15}
                 >
-                  Contact Me
+                  All Skills
                 </Button>
-              </Buttons>
-            </HeroBanner>
-            <HeroBody>
-              <NavCard
-                height={200}
-                width={275}
-                stemDir="h"
-                stemLoc={1}
-                color1={theme.blue.get()}
-                color2={theme.purple.get()}
-                effectLocation={{ x: 0.5, y: 0.8 }}
-                id={"welcomeCard"}
-              >
-                <CardContent>
-                  <p>Hello</p>
-                </CardContent>
-              </NavCard>
-            </HeroBody>
-          </HeroSection>
-          <Skills>
-            <SkillsCard skills={skills} />
-          </Skills>
-        </Container>
-      )}
+              </SkillsCard>
+            </Skills>
+            <Skills>
+              <ProjectsCard selected={selected} />
+            </Skills>
+          </>
+        )}
+      </Container>
     </Layout>
   );
 }
