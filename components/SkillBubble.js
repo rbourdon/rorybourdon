f398ebd14aee2da8738075f06aa4dd3f3571f097
Rory Bourdon <rorybourdon@gmail.com>
2021-09-22 09:27:50 -0400

    Add hover to skill bubble and refactor

--- components/SkillBubble.js
+++ components/SkillBubble.js
@@ -1,10 +1,14 @@
 import styled, { ThemeContext } from "styled-components";
-import { motion, useTransform } from "framer-motion";
+import {
+  animate,
+  motion,
+  transform,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
 import { useContext } from "react";
-
 const Bubble = styled(motion.div)`
   height: 36px;
-  width: max-content;
   min-width: 75px;
   padding: 0 22px;
   font-weight: 300;
@@ -13,32 +17,44 @@ const Bubble = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: center;
+  user-select: none;
 `;
 
-const bubbleV = {
-  hidden: { y: 45, opacity: 0 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.3,
-    },
-  },
+const handleHover = (hover, to) => {
+  animate(hover, to, { type: "tween", duration: 0.2 });
 };
 
-export default function SkillBubble({ title, pos }) {
+export default function SkillBubble({ title, bubbleV, transition }) {
   const theme = useContext(ThemeContext);
+  const hover = useMotionValue(0);
   const border = useTransform(
     theme.primary_dark,
     (latestColor1) => "1px solid  " + latestColor1
   );
+  const backgroundColor = useTransform(
+    [theme.primary_light, theme.blue, theme.teal, hover],
+    ([latestColor1, latestColor2, latestColor3, latestHover]) =>
+      transform(
+        latestHover,
+        [0, 1],
+        [
+          latestColor1,
+          transform(latestHover, [0, 3], [latestColor2, latestColor3]),
+        ]
+      )
+  );
+
   return (
     <Bubble
       key={title + "_bub"}
       variants={bubbleV}
-      layout
-      custom={pos}
-      style={{ border }}
+      layoutId={`${title.substring(0, 3)}Bubble`}
+      transition={transition ? transition : { type: "spring", stiffness: 30 }}
+      onHoverStart={() => handleHover(hover, 1)}
+      onHoverEnd={() => handleHover(hover, 0)}
+      style={{ border, backgroundColor }}
+      initial="hidden"
+      animate="visible"
     >
       {title}
     </Bubble>
