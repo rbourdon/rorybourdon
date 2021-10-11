f249b235ccbebfd97f212be4592051aa2ac31edc
Rory Bourdon <rorybourdon@gmail.com>
2021-10-11 13:22:20 -0400

    First pass of projects card

--- components/ProjectsCard.js
+++ components/ProjectsCard.js
@@ -2,36 +2,61 @@ import styled, { ThemeContext } from "styled-components";
 import { motion } from "framer-motion";
 import NavCard from "@/components/NavCard";
 import { useContext } from "react";
+import ProjectsIcon from "@/components/ProjectsIcon";
+import Underline from "@/components/Underline";
+import ProjectSummary from "./ProjectSummary";
+import { useInView } from "react-intersection-observer";
 import Button from "@/components/Button";
 
 const CardContent = styled(motion.div)`
   width: 100%;
   height: 100%;
   display: grid;
-  grid-template-columns: 100%;
-  grid-template-rows: 35% 10% 1fr calc(10% + 40px);
+  grid-template-columns: 0.85fr 1fr;
+  grid-template-rows: 10% 90%;
   justify-items: center;
   align-items: center;
   position: relative;
-  padding: 35px 30px;
+  padding: 35px 40px;
   z-index: 5;
 `;
 
 const Icon = styled(motion.div)`
-  width: 90%;
+  width: 100%;
   height: 100%;
   display: flex;
   align-items: center;
+  justify-content: center;
+  padding: 0 20px 0 0;
 `;
 
 const Label = styled(motion.p)`
   width: 100%;
+  height: 100%;
   font-size: 1.35rem;
   font-weight: 300;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  grid-column: span 2;
 `;
 
-const skillsCardV = {
+const ProjectsBox = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  font-size: 1.35rem;
+  font-weight: 300;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 0 0 0 20px;
+  overflow: hidden;
+`;
+
+const projectsCardV = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
@@ -44,50 +69,94 @@ const skillsCardV = {
 
 const iconV = {
   hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.03,
+      duration: 0.05,
+      delay: 710 * 230 * 0.0000012 + 2.45,
+      when: "beforeChildren",
+    },
+  },
+};
+
+const underlineV = {
+  hidden: {
     pathLength: 0,
   },
   visible: {
     pathLength: 1,
     transition: {
-      delay: 710 * 230 * 0.0000012 + 2.15,
-      duration: 0.65,
+      delay: 710 * 230 * 0.0000012 + 2.05,
+      duration: 0.8,
+      ease: "easeInOut",
     },
   },
 };
 
 export default function ProjectsCard({ selected }) {
   const theme = useContext(ThemeContext);
+  const { ref, inView } = useInView({
+    threshold: 0.66,
+  });
   return (
-    <NavCard
-      height={510}
-      width={710}
-      stemDir="h"
-      stemLoc={7}
-      stemLength={475}
-      color1={theme.teal.get()}
-      color2={theme.green.get()}
-      effectOffset={{ x: 600, y: 20 }}
-      effectRadius={375}
-      id={"projectsCard"}
-      delay={0.5}
-      tagline="I have a wide variety of skills and love to learn."
-      selected={selected}
+    <motion.div
+      initial="hidden"
+      animate={selected ? "selected" : inView ? "visible" : "hidden"}
+      exit={selected ? null : "exit"}
     >
-      <CardContent variants={skillsCardV}>
-        <Icon variants={iconV}></Icon>
-        <Label>Projects</Label>
-        <Button
-          width={150}
-          height={50}
-          color1={theme.teal.get()}
-          color2={theme.green.get()}
-          href="/"
-          id="projectsButton"
-          delay={710 * 230 * 0.0000012 + 2.15}
-        >
-          All Projects
-        </Button>
-      </CardContent>
-    </NavCard>
+      <NavCard
+        height={422}
+        width={627}
+        stemDir="h"
+        stemLoc={7}
+        stemLength={375}
+        color1={theme.teal}
+        color2={theme.green}
+        effectOffset={{ x: 600, y: 20 }}
+        effectRadius={315}
+        id="projects"
+        delay={0.5}
+        tagline="I enjoy building and experimenting"
+        intersectionRef={ref}
+        stem
+      >
+        <CardContent variants={projectsCardV}>
+          <Label>
+            Projects
+            <Underline
+              variants={underlineV}
+              width={130}
+              sWidth={1.2}
+              color1={theme.teal}
+              color2={theme.green}
+              id="projects"
+            />
+          </Label>
+          <Icon
+            layoutId={"projectsCard_icon"}
+            style={{ color: theme.primary_verydark }}
+          >
+            <ProjectsIcon iconV={iconV} delay={710 * 230 * 0.0000012 + 2.15} />
+          </Icon>
+          <ProjectsBox>
+            <ProjectSummary />
+            <Button
+              width={150}
+              height={50}
+              color1={theme.teal}
+              color2={theme.green}
+              href="/projects"
+              id="projects"
+              delay={710 * 230 * 0.0000012 + 2.15}
+            >
+              All Projects
+            </Button>
+          </ProjectsBox>
+        </CardContent>
+      </NavCard>
+    </motion.div>
   );
 }
