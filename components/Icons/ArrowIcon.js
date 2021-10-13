62f50a4c00b1384cbf616cdb6adcc19cfe36e249
Rory Bourdon <rorybourdon@gmail.com>
2021-10-13 10:09:38 -0400

    Add arrow icon

---
+++ components/Icons/ArrowIcon.js
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+
+export default function ArrowIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12.4 15.5">
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+      >
+        <motion.path d="M6.2 15V1.9" />
+        <motion.path d="M.5 6.2 6.1.5a.1.1 0 0 1 .2 0l5.6 5.7" />
+      </g>
+    </svg>
+  );
+}
