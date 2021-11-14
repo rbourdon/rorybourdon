638fcf9926bee000328cdacd8eee02943f5009fb
Rory Bourdon <rorybourdon@gmail.com>
2021-11-13 23:01:45 -0500

    Improve horizoncircle place/sizing

--- components/Icons/HorizonCircle.js
+++ components/Icons/HorizonCircle.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import styled, { ThemeContext } from "styled-components";
 import useWindowSize from "@/components/utils/useWindowSize";
 
-const Circle = styled(motion.svg)`
+const Circle = styled.svg`
   position: absolute;
   top: 0;
   left: 0;
@@ -14,22 +14,24 @@ const circleV = {
   hidden: {
     pathLength: 1,
     pathOffset: 1,
+    pathSpacing: 1,
   },
-  visible: {
-    pathOffset: [1, 0.25, 1],
-    pathSpacing: [0, 0.9, 0],
-    rotate: [0, 180, 360],
+  visible: (dir) => ({
+    pathOffset: 0.1,
+    pathSpacing: 0.15,
+    rotate: [0, 720 * dir],
     pathLength: 1,
     transition: {
       type: "tween",
-      duration: 12,
+      duration: 42,
       repeat: Infinity,
       ease: "linear",
+      repeatType: "mirror",
     },
-  },
+  }),
 };
 
-export default function HorizonCircle({ cx = "92%", cy = "15%", r = 330 }) {
+export default function HorizonCircle({ cx = "50%", cy = "50%", r = 0.5 }) {
   const theme = useContext(ThemeContext);
   const { width = 330 } = useWindowSize();
   return (
@@ -37,22 +39,27 @@ export default function HorizonCircle({ cx = "92%", cy = "15%", r = 330 }) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox={`0 0 400 400`}
       fill="none"
+      width="100%"
+      height="100%"
     >
       <motion.circle
         cx={cx}
         cy={cy}
-        r={r - width * 0.1}
-        stroke={theme.primary_slightlydark}
+        r={r * 550 - r * (width * 0.08) * 0.965}
+        variants={circleV}
+        custom={-1}
         strokeWidth={1}
+        stroke={theme.primary_slightlydark}
         vectorEffect="non-scaling-stroke"
       />
       <motion.circle
         cx={cx}
         cy={cy}
-        r={r - width * 0.101}
+        r={r * 550 - r * (width * 0.08)}
+        custom={1}
         variants={circleV}
-        stroke={theme.primary_slightlydark}
         strokeWidth={1}
+        stroke={theme.primary_slightlydark}
         vectorEffect="non-scaling-stroke"
       />
     </Circle>
