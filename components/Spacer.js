7245158845dec62c7affaf796c5a89460d598602
Rory Bourdon <rorybourdon@gmail.com>
2021-11-13 23:08:58 -0500

    Make spacer use div instead of span

--- components/Spacer.js
+++ components/Spacer.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
-const Container = styled.span`
+const Container = styled.div`
   width: ${(props) => props.$width + "px"};
   height: ${(props) => props.$height + "px"};
-  display: block;
 `;
 
-export default function Spacer({ width = 50, height = 50 }) {
+export default function Spacer({ width = 5, height = 5 }) {
   return <Container $width={width} $height={height} />;
 }
