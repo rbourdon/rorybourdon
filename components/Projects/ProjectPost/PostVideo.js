26888273a88bb9701de23d823e5c72dc5ab0faf9
Rory Bourdon <rorybourdon@gmail.com>
2021-11-15 23:03:26 -0500

    Show placeholder for post videos

--- components/Projects/ProjectPost/PostVideo.js
+++ components/Projects/ProjectPost/PostVideo.js
@@ -1,24 +1,65 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const Container = styled(motion.figure)`
   width: 100%;
   height: max-content;
+  margin: 2vw 0;
+  position: relative;
+`;
+
+const Video = styled(motion.video)`
+  width: 100%;
+  height: max-content;
+  display: block;
+`;
+
+const PlaceholderImage = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  display: block;
+  background-color: black;
+  z-index: 2;
 `;
 
 export default function PostVideo({
   src,
+  placeholder,
   alt = "Sorry, your browser doesn't support embedded videos.",
   children,
 }) {
+  const [videoReady, setVideoReady] = useState(false);
+  const [allowPlay, setAllowPlay] = useState(false);
+
+  useEffect(() => {
+    setAllowPlay(true);
+  }, []);
+
   return (
     <Container>
-      {src && (
-        <video style={{ width: "100%", maxHeight: "100%" }} autoPlay loop muted>
+      {src && allowPlay && (
+        <Video
+          style={{
+            width: "100%",
+            maxHeight: "100%",
+          }}
+          autoPlay
+          loop
+          muted
+          playsInline
+          onCanPlayThrough={() => setVideoReady(true)}
+        >
           <source src={src} type="video/mp4" />
           {alt}
-        </video>
+        </Video>
       )}
+      <PlaceholderImage style={{ opacity: videoReady ? 0 : 1 }}>
+        {placeholder}
+      </PlaceholderImage>
+
       {children}
     </Container>
   );
