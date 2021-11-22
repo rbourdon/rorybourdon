40cd0a1dbc1391853992f22b4828d882d8d2b4fb
Rory Bourdon <rorybourdon@gmail.com>
2021-11-22 18:20:25 -0500

    Add project and socials card background effects

---
+++ components/Projects/ProjectsCard/ProjectsBackgroundEffect.js
@@ -0,0 +1,131 @@
+import { motion, useMotionValue, useViewportScroll } from "framer-motion";
+import { useState, useContext, useEffect } from "react";
+import styled, { ThemeContext } from "styled-components";
+
+const Container = styled(motion.div)`
+  position: absolute;
+  left: 0;
+  pointer-events: none;
+  top: 0;
+`;
+
+const containerV = {
+  hidden: (custom) => ({
+    opacity: 0,
+    y: custom + 150,
+    transition: {
+      staggerChildren: 0.1,
+      type: "tween",
+      duration: 0.2,
+      y: {
+        type: "spring",
+        stiffness: 40,
+        mass: 1.3,
+        damping: 8,
+      },
+    },
+  }),
+  visible: (custom) => ({
+    opacity: 1,
+    y: custom,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.25,
+      type: "tween",
+      duration: 0.2,
+      y: {
+        type: "spring",
+        stiffness: 40,
+        mass: 1.3,
+        damping: 8,
+      },
+    },
+  }),
+  selected: {
+    opacity: 0,
+    transition: {
+      staggerChildren: 0.05,
+      type: "tween",
+      duration: 0.35,
+    },
+  },
+};
+
+const gearV = {
+  hidden: {
+    transition: {
+      type: "tween",
+      duration: 0.5,
+      ease: "linear",
+    },
+  },
+  visible: (custom) => ({
+    rotate: [0, 360],
+    transition: {
+      type: "tween",
+      duration: 35 * (custom / 2),
+      ease: "linear",
+      repeat: Infinity,
+    },
+  }),
+  selected: {
+    transition: {
+      type: "tween",
+      duration: 0.5,
+      ease: "linear",
+    },
+  },
+};
+
+export default function ProjectsBackgroundEffect({
+  inView = false,
+  style = { x: 0, y: 0, scale: 1, delay: 0 },
+}) {
+  const [animating, setAnimating] = useState(true);
+  const theme = useContext(ThemeContext);
+  const { scrollY } = useViewportScroll();
+  const x = useMotionValue(style.x);
+  const y = useMotionValue(style.y + 150);
+
+  const scale = useMotionValue(style.scale);
+
+  useEffect(() => {
+    const unsubscribeY = scrollY.onChange((progress) => {
+      inView &&
+        !animating &&
+        y.set(
+          y.get() - (progress - scrollY.prev) * 0.55 * (style.scale - 0.25)
+        );
+    });
+    return () => {
+      unsubscribeY();
+    };
+  }, [animating, inView, scrollY, style.scale, y]);
+
+  return (
+    <Container
+      style={{ x, y, scale }}
+      variants={containerV}
+      custom={style.y}
+      onAnimationStart={() => setAnimating(true)}
+      onAnimationComplete={() => setAnimating(false)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-1 -1 170 170"
+        width={170}
+        height={170}
+      >
+        <motion.path
+          variants={gearV}
+          fill={theme.primary_light}
+          stroke={theme.primary_slightlydark}
+          strokeMiterlimit="10"
+          strokeWidth=".75"
+          custom={style.scale}
+          d="M157.49 67.72a9.72 9.72 0 0 1-9-6l-3-7.29a9.73 9.73 0 0 1 2.11-10.59 10.36 10.36 0 0 0 0-14.66l-8.54-8.54a10.37 10.37 0 0 0-14.65 0 9.72 9.72 0 0 1-10.6 2.11l-7.29-3a9.72 9.72 0 0 1-6-9A10.36 10.36 0 0 0 90.15.38H78.08a10.36 10.36 0 0 0-10.36 10.36 9.71 9.71 0 0 1-6 9l-7.29 3a9.71 9.71 0 0 1-10.59-2.11 10.38 10.38 0 0 0-14.66 0l-8.54 8.54a10.38 10.38 0 0 0 0 14.66 9.71 9.71 0 0 1 2.11 10.59l-3 7.29a9.71 9.71 0 0 1-9 6A10.36 10.36 0 0 0 .38 78.08v12.07a10.36 10.36 0 0 0 10.36 10.37 9.72 9.72 0 0 1 9 6l3 7.29a9.73 9.73 0 0 1-2.11 10.6 10.37 10.37 0 0 0 0 14.65l8.54 8.54a10.36 10.36 0 0 0 14.66 0 9.73 9.73 0 0 1 10.59-2.11l7.29 3a9.72 9.72 0 0 1 6 9 10.36 10.36 0 0 0 10.36 10.37h12.08a10.37 10.37 0 0 0 10.37-10.37 9.73 9.73 0 0 1 6-9l7.29-3a9.74 9.74 0 0 1 10.6 2.11 10.35 10.35 0 0 0 14.65 0l8.54-8.54a10.35 10.35 0 0 0 0-14.65 9.75 9.75 0 0 1-2.11-10.6l3-7.29a9.73 9.73 0 0 1 9-6 10.37 10.37 0 0 0 10.37-10.37V78.08a10.36 10.36 0 0 0-10.37-10.36Zm-73.37 49.92a33.53 33.53 0 1 1 33.52-33.52 33.53 33.53 0 0 1-33.52 33.52Z"
+        />
+      </svg>
+    </Container>
+  );
+}
---
+++ components/SocialsCard/SocialsBackgroundEffect.js
@@ -0,0 +1,147 @@
+import { motion, useMotionValue, useViewportScroll } from "framer-motion";
+import { useState, useContext, useEffect } from "react";
+import styled, { ThemeContext } from "styled-components";
+
+const Container = styled(motion.div)`
+  position: absolute;
+  left: 0;
+  pointer-events: none;
+  top: 0;
+`;
+
+const containerV = {
+  hidden: (custom) => ({
+    opacity: 0,
+    y: custom.y + 150,
+    scale: custom.scale,
+    transition: {
+      staggerChildren: 0.1,
+      type: "tween",
+      duration: 0.2,
+      y: {
+        type: "spring",
+        stiffness: 40,
+        mass: 1.3,
+        damping: 8,
+      },
+    },
+  }),
+  visible: (custom) => ({
+    opacity: 1,
+    y: custom.y,
+    scale: custom.scale,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.25,
+      type: "tween",
+      duration: 0.2,
+      y: {
+        type: "spring",
+        stiffness: 40,
+        mass: 1.3,
+        damping: 8,
+      },
+    },
+  }),
+  selected: {
+    opacity: 0,
+    transition: {
+      staggerChildren: 0.05,
+      type: "tween",
+      duration: 0.35,
+    },
+  },
+};
+
+const lineV = {
+  hidden: {
+    scaleX: 0,
+    originX: 0,
+    transition: {
+      type: "tween",
+      duration: 0.5,
+      ease: "linear",
+    },
+  },
+  visible: {
+    scaleX: 1,
+    originX: 0,
+    transition: {
+      type: "tween",
+      duration: 0.8,
+      ease: "easeInOut",
+    },
+  },
+  selected: {
+    scaleX: 0,
+    originX: 0,
+    transition: {
+      type: "tween",
+      duration: 0.15,
+      ease: "linear",
+    },
+  },
+};
+
+export default function SocialsBackgroundEffect({
+  inView = false,
+  style = { x: 0, y: 0, scale: 1, delay: 0 },
+}) {
+  const [animating, setAnimating] = useState(true);
+  const theme = useContext(ThemeContext);
+  const { scrollY } = useViewportScroll();
+  const x = useMotionValue(style.x);
+  const y = useMotionValue(style.y + 150);
+
+  const scale = useMotionValue(style.scale);
+
+  useEffect(() => {
+    const unsubscribeY = scrollY.onChange((progress) => {
+      inView &&
+        !animating &&
+        y.set(
+          y.get() - (progress - scrollY.prev) * 0.55 * (style.scale - 0.25)
+        );
+    });
+    return () => {
+      unsubscribeY();
+    };
+  }, [animating, inView, scrollY, style.scale, y]);
+
+  return (
+    <Container
+      style={{ x, y, scale }}
+      variants={containerV}
+      custom={style}
+      onAnimationStart={() => setAnimating(true)}
+      onAnimationComplete={() => setAnimating(false)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-1 -1 130 130"
+        width={130}
+        height={130}
+      >
+        <path
+          fill="#b3b1b5"
+          d="M63.86 127.53h-.09a63.75 63.75 0 1 1 44-17.63l12.83 14.38-.66.28c-10.64 4.51-21.41-1.64-25.81-4.72a63.58 63.58 0 0 1-30.27 7.69ZM63.77 1a62.75 62.75 0 1 0 30.16 117.79l.29-.15.26.18c4 2.85 14.25 8.93 24.41 5.06l-12.53-14 .36-.34A62.74 62.74 0 0 0 63.77 1Z"
+        />
+        <motion.path
+          variants={lineV}
+          fill="#a3ba5a"
+          d="M83.52 53.33c-13.09-.13-26.17-.37-39.26-.54q-6.54-.09-13.09-.16c-3.76 0-3.77-5.88 0-5.85 13.09.14 26.17.38 39.26.55l13.09.16c3.75.04 3.76 5.88 0 5.84Z"
+        />
+        <motion.path
+          variants={lineV}
+          fill={theme.green}
+          d="M97.85 67 45 66.42l-15-.15c-3.76 0-3.77-5.89 0-5.85l52.81.58 15 .16c3.8.01 3.81 5.84.04 5.84Z"
+        />
+        <motion.path
+          variants={lineV}
+          fill="#a3ba5a"
+          d="M97.85 80.61 45 80.06l-15-.15c-3.76 0-3.77-5.89 0-5.85l52.89.55 15 .16c3.72.03 3.73 5.88-.04 5.84Z"
+        />
+      </svg>
+    </Container>
+  );
+}
