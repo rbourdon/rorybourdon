e527c2a63cac9dde8412b9ae1579b6a4e76e0183
Rory Bourdon <rorybourdon@gmail.com>
2021-11-28 08:39:05 -0500

    Add favicons

--- components/layout.js
+++ components/layout.js
@@ -18,8 +18,29 @@ function Layout({ children }) {
     <Container style={{ backgroundColor: theme.primary_light }}>
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/ekd5pcq.css" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="mask-icon" href="/favicon.ico" color="#FFFFFF" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
+
         <meta name="author" content="Rory Bourdon" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
