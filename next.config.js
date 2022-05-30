490b15865fc223b12cb53f6651ed584f1364ab5d
Rory Bourdon <rorybourdon@gmail.com>
2022-05-30 11:13:09 -0400

    Update deps

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   formats: ["image/avif", "image/webp"],
+  compiler: {
+    styledComponents: true,
+  },
   images: {
     dangerouslyAllowSVG: true,
     domains: ["media.graphcms.com", "media.graphassets.com"],
