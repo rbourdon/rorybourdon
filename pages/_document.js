c333cdde251a820662681f34e8ef5167214393c4
Rory Bourdon <rorybourdon@gmail.com>
2022-07-09 13:25:01 -0400

    Improve fonts

--- pages/_document.js
+++ pages/_document.js
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,800;1,100;1,200;1,300;1,400;1,800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,800;1,100;1,200;1,300;1,400;1,800&display=swap"
             rel="stylesheet"
           />
         </Head>
